927d8528dadb9ea2e2f4ba3cd3e2a7b3e0fa820c
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-25 06:13:32 -0500

    initial route added

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.listen(process.env.PORT ?? 3000);
-console.log("Server is running in port", process.env.PORT ?? 3000);
+app.get("/", (req, res) => res.send("Hello Wolrd"));
+
+app.listen(process.env.PORT || 3000);
+console.log("Server is running in port", process.env.PORT || 3000);
