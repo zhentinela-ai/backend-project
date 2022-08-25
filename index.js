ba50a4cabf72b213eb7031eafd1415759a9cce05
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-25 07:12:03 -0500

    users query added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const { join } = require("path");
 const { connectDB } = require("./db");
+const User = require("./models/users");
 
 connectDB();
 const app = express();
@@ -11,9 +12,10 @@ app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Routes
-app.get("/api/users", (req, res) =>
-  res.json([{ name: "ryan" }, { name: "joe" }])
-);
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
 
 app.get("/profile", (req, res) => {
   res.render("profile", {
