3bc763b601d1f44b4cfae8ebe777766a7d97a496
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-25 06:30:59 -0500

    template engine added & restAPI

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const { join } = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Wolrd"));
+// Settings
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
 
+// Routes
+app.get("/api/users", (req, res) =>
+  res.json([{ name: "ryan" }, { name: "joe" }])
+);
+
+app.get("/profile", (req, res) => {
+  res.render("profile", {
+    name: "ryan",
+    age: 27
+  });
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Starting the server
 app.listen(process.env.PORT || 3000);
-console.log("Server is running in port", process.env.PORT || 3000);
+console.log("Server is running on port", process.env.PORT || 3000);
